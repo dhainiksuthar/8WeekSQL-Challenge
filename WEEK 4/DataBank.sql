c4b344e537a142e755490b88d71c6f8731146f3a
Dhainik Suthar <suthardhainik188@gmail.com>
2023-04-13 11:12:28 +0530

    Section C some code added

--- WEEK 4/DataBank.sql
+++ WEEK 4/DataBank.sql
@@ -100,7 +100,7 @@ WITH cte1 AS(
 ),
 
 cte2 AS(
-	SELECT customer_id, month, amount + COALESCE(LAG(amount) OVER(PARTITION BY customer_id ORDER BY month), 0) AS closing_amount, 
+	SELECT customer_id, month, amount, amount + COALESCE(LAG(amount) OVER(PARTITION BY customer_id ORDER BY month), 0) AS closing_amount, 
 	COALESCE(LEAD(month) OVER(PARTITION BY customer_id ORDER BY month), 5) AS next_month
 	FROM cte1
 ),
@@ -161,4 +161,32 @@ FROM (
 
 
 
+
+										--C. Data Allocation Challenge
+--To test out a few different hypotheses - the Data Bank team wants to run an experiment where different groups of customers would be allocated data using 3 different options:
+
+--Option 1: data is allocated based off the amount of money at the end of the previous month
+--Option 2: data is allocated on the average amount of money kept in the account in the previous 30 days
+--Option 3: data is updated real-time
+--For this multi-part challenge question - you have been requested to generate the following data elements to help the Data Bank team estimate how much data will need to be provisioned for each option:
+
+--running customer balance column that includes the impact each transaction
+--customer balance at the end of each month
+--minimum, average and maximum values of the running balance for each customer
+--Using all of the data available - how much data would have been required for each option on a monthly basis?
+
+--running customer balance column that includes the impact each transaction
+
+SELECT 
+	*,
+	CASE WHEN LAG(
+
+--customer balance at the end of each month
+WITH cte1 AS(
+SELECT
+	customer_id, MAX(txn_date) AS 
+FROM #impact_transaction
+GROUP BY customer_id)
+
+SELECT * FROM cte1 WHERE (customer_id, 
 
