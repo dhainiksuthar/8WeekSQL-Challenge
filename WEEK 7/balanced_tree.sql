db8930b9cad33b0783875dc93ef574a0015f2264
Dhainik Suthar <suthardhainik188@gmail.com>
2023-04-19 17:53:19 +0530

    Wee7 A and B completed C Started

---
+++ WEEK 7/balanced_tree.sql
@@ -0,0 +1,262 @@
+				-- A High Level Sales Analysis
+
+-- A1. What was the total quantity sold for all products?
+
+SELECT
+	SUM(qty) AS sold_quantity
+FROM
+	balanced_tree.sales;
+
+
+-- A2. What is the total generated revenue for all products before discounts?
+
+SELECT
+	SUM(qty*price) AS revenue
+FROM
+	balanced_tree.sales;
+
+
+-- A3. What was the total discount amount for all products?
+
+SELECT
+	SUM(discount) AS total_discount
+FROM
+	balanced_tree.sales;
+
+
+								-- B Transaction Analysis
+
+-- B1. How many unique transactions were there?
+
+SELECT 
+	COUNT(DISTINCT txn_id) AS transaction_count 
+FROM balanced_tree.sales;
+
+-- B2. What is the average unique products purchased in each transaction?
+
+WITH cte1 AS(
+SELECT 
+	COUNT(DISTINCT prod_id) AS avg_qty
+FROM balanced_tree.sales
+GROUP BY txn_id)
+
+SELECT
+	AVG(avg_qty) AS avg_qty
+FROM
+	cte1;
+
+-- B3. What are the 25th, 50th and 75th percentile values for the revenue per transaction?
+
+WITH cte1 AS(
+	SELECT
+		CAST(SUM((100.0-discount)*qty*price/100.0) AS DECIMAL(10,2)) AS amount
+	FROM 
+		balanced_tree.sales
+GROUP BY txn_id
+)
+
+SELECT
+	PERCENTILE_CONT(0.25) WITHIN GROUP(ORDER BY amount) OVER(PARTITION BY NULL) AS [25th_percentile],
+	PERCENTILE_CONT(0.5) WITHIN GROUP(ORDER BY amount) OVER(PARTITION BY NULL) AS [50th_percentile],
+	PERCENTILE_CONT(0.75) WITHIN GROUP(ORDER BY amount) OVER(PARTITION BY NULL) AS [75th_percentile]
+FROM cte1;
+
+-- B4. What is the average discount value per transaction?
+
+SELECT
+	txn_id, CAST(AVG((100.0-discount)*qty*price/100.0) AS DECIMAL(5,2)) AS avg_discount
+FROM
+	balanced_tree.sales
+GROUP BY txn_id;
+
+-- B5. What is the percentage split of all transactions for members vs non-members?
+WITH cte1 AS(
+	SELECT
+		SUM(CASE WHEN member = 't' THEN 1 ELSE 0 END) AS member,
+		SUM(CASE WHEN member = 'f' THEN 1 ELSE 0 END) AS nonmember
+	FROM
+		balanced_tree.sales
+)
+
+SELECT
+	CAST(member*100.0/(nonmember+member) AS DECIMAL(4,2)) AS member_percentage,
+	CAST(nonmember*100.0/(nonmember+member) AS DECIMAL(4,2)) AS member_percentage
+FROM cte1;
+
+-- B6. What is the average revenue for member transactions and non-member transactions?
+
+WITH cte1 AS (
+	SELECT txn_id, member,
+		SUM( (100.0-discount)*qty*price/100.0 ) AS sum
+	FROM balanced_tree.sales
+	GROUP BY txn_id, member
+)
+
+SELECT 
+	member, AVG(sum) AS avg_revenue 
+FROM cte1
+GROUP BY member;
+
+									-- C Product Analysis
+
+-- C1. What are the top 3 products by total revenue before discount?
+
+SELECT TOP 3
+	product_name, SUM(qty*s.price) AS revenue
+FROM balanced_tree.sales s
+JOIN balanced_tree.product_details p ON s.prod_id = p.product_id
+GROUP BY product_name
+ORDER BY revenue DESC;
+
+-- C2. What is the total quantity, revenue and discount for each segment?
+
+SELECT 
+	segment_name, SUM(qty) AS quantity, SUM(qty*s.price) AS revenue, SUM(discount*qty*s.price/100.0) AS discount
+FROM 
+	balanced_tree.sales s
+LEFT JOIN 
+	balanced_tree.product_details p 
+ON 
+	s.prod_id = p.product_id
+GROUP BY 
+	segment_name
+ORDER BY segment_name
+
+-- C3. What is the top selling product for each segment?
+
+WITH cte1 AS(
+	SELECT 
+		segment_name, product_name, SUM(qty) AS quantity, SUM(qty*s.price) AS revenue, SUM(discount*qty*s.price/100.0) AS discount
+	FROM 
+		balanced_tree.sales s
+	LEFT JOIN 
+		balanced_tree.product_details p 
+	ON 
+		s.prod_id = p.product_id
+	GROUP BY 
+		segment_name, product_name
+)
+SELECT *
+FROM (
+	SELECT *,
+		RANK() OVER(PARTITION BY segment_name ORDER BY quantity DESC) as rank
+	FROM cte1
+) A
+WHERE A.rank = 1;
+
+-- C4. What is the total quantity, revenue and discount for each category?
+
+SELECT 
+	category_name, SUM(qty) AS quantity, SUM(qty*s.price) AS revenue, SUM(discount*qty*s.price/100.0) AS discount
+FROM 
+	balanced_tree.sales s
+LEFT JOIN 
+	balanced_tree.product_details p 
+ON 
+	s.prod_id = p.product_id
+GROUP BY 
+	category_name
+ORDER BY category_name
+
+-- C5. What is the top selling product for each category?
+
+WITH cte1 AS(
+	SELECT 
+		category_name, product_name, SUM(qty) AS quantity, SUM(qty*s.price) AS revenue, SUM(discount*qty*s.price/100.0) AS discount
+	FROM 
+		balanced_tree.sales s
+	LEFT JOIN 
+		balanced_tree.product_details p 
+	ON 
+		s.prod_id = p.product_id
+	GROUP BY 
+		category_name, product_name
+)
+SELECT *
+FROM (
+	SELECT *,
+		RANK() OVER(PARTITION BY category_name ORDER BY quantity DESC) as rank
+	FROM cte1
+) A
+WHERE A.rank = 1;
+
+-- C6. What is the percentage split of revenue by product for each segment?
+
+WITH cte1 AS(
+	SELECT 
+		segment_name, product_name, SUM((100.0-discount)*qty*s.price) AS product_revenue
+	FROM 
+		balanced_tree.sales s
+	LEFT JOIN 
+		balanced_tree.product_details p 
+	ON 
+		s.prod_id = p.product_id
+	GROUP BY 
+		segment_name, product_name
+),
+cte2 AS(
+	SELECT *,
+		SUM(product_revenue) OVER(PARTITION BY segment_name) as segment_revenue
+	FROM cte1
+)
+SELECT segment_name, product_name, 
+	CAST(product_revenue*100.0/segment_revenue AS DECIMAL(4,2)) AS product_percentage
+FROM cte2
+ORDER BY segment_name, product_percentage DESC
+
+
+-- C7. What is the percentage split of revenue by segment for each category?
+
+WITH cte1 AS(
+	SELECT 
+		segment_name, category_name, SUM((100.0-discount)*qty*s.price) AS category_revenue
+	FROM 
+		balanced_tree.sales s
+	LEFT JOIN 
+		balanced_tree.product_details p 
+	ON 
+		s.prod_id = p.product_id
+	GROUP BY 
+		segment_name, category_name
+),
+cte2 AS(
+	SELECT *,
+		SUM(category_revenue) OVER(PARTITION BY category_name) as segment_revenue
+	FROM cte1
+)
+SELECT segment_name, category_name, 
+	CAST(category_revenue*100.0/segment_revenue AS DECIMAL(4,2)) AS category_percentage
+FROM cte2
+ORDER BY segment_name, category_percentage DESC
+
+-- C8. What is the percentage split of total revenue by category?
+
+SELECT category_name, 
+	CAST(SUM((100.0-discount)*qty*s.price) * 100.0 / SUM(SUM((100.0-discount)*qty*s.price)) OVER() AS DECIMAL(4,2)) AS Percentage
+FROM balanced_tree.sales s
+JOIN balanced_tree.product_details p ON s.prod_id = p.product_id
+GROUP BY category_name
+
+-- C9. What is the total transaction “penetration” for each product? 
+--(hint: penetration = number of transactions where at least 1 quantity of a product was purchased divided by total number of transactions)
+
+SELECT product_name, 
+	CAST(COUNT(*)*100.0/(SELECT COUNT(DISTINCT txn_id) FROM balanced_tree.sales) AS DECIMAL(4,2)) AS Percentage
+FROM balanced_tree.sales s
+LEFT JOIN balanced_tree.product_details p ON s.prod_id = p.product_id
+GROUP BY product_name
+ORDER BY Percentage DESC;
+
+
+-- C10. What is the most common combination of at least 1 quantity of any 3 products in a 1 single transaction?
+
+WITH cte1 AS(
+SELECT *, ROW_NUMBER() AS unique_id OVER(ORDER BY start_txn_time) FROM balanced_tree.sales
+)
+
+
+
+
+SELECT * FROM balanced_tree.product_details;
+
+SELECT * FROM balanced_tree.sales;
